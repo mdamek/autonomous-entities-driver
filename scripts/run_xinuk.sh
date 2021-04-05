d4282a8544d9dedea505903e8a8119a72f7244d1
MarcinDamek <marcind70@gmail.com>
2021-04-05 17:22:34 +0200

    add shapes to scripts

--- scripts/run_xinuk.sh
+++ scripts/run_xinuk.sh
@@ -2,33 +2,84 @@
 source raspberry_hosts.sh
 
 USERNAME="pi"
+APPLICATION_NAME="mock"
+SHAPE="square"
 
-APLICATION_NAME="mock"
+while test $# -gt 0; do
+    case "$1" in
+    -shape)
+        shift
+        SHAPE=$1
+        shift
+        ;;
+    -name)
+        shift
+        APPLICATION_NAME=$1
+        shift
+        ;;
+    *)
+        echo "$1 is not a recognized flag!"
+        exit 0
+        ;;
+    esac
+done
 
-WORLD_WIDTH=128
-WORLD_HEIGHT=32
+WORLD_WIDTH=0
+WORLD_HEIGHT=0
+WORKERS_X=0
+WORKERS_Y=0
+
+case ${SHAPE} in
+"square")
+    WORLD_WIDTH=64
+    WORLD_HEIGHT=64
+    WORKERS_X=2
+    WORKERS_Y=2
+    ;;
+"vertical")
+    WORLD_WIDTH=32
+    WORLD_HEIGHT=128
+    WORKERS_X=1
+    WORKERS_Y=4
+    ;;
+"horizontal")
+    WORLD_WIDTH=128
+    WORLD_HEIGHT=32
+    WORKERS_X=4
+    WORKERS_Y= 1
+    ;;
+*)
+    echo "Define the shape of the space with flag -shape: square, horizontal or vertical"
+    exit 0
+    ;;
+esac
+
+if [ ${APPLICATION_NAME} != mock ]; then
+    echo ${APPLICATION_NAME}
+    echo "Define application name with flag -name: mock"
+    exit 0
+fi
+
 ITERATIONS_NUMBER=100
+SIGNAL_DISABLED=true
+
 MIN_NR_OF_MEMBERS=4
 GUI_TYPE="ledPanel"
 LED_PANEL_PORT=8000
-WORKERS_ROOT=2
-WORKERS_X=4
-WORKERS_Y=1
 XINUK_PORT=2551
-SIGNAL_DISABLED=true
+WORKERS_ROOT=2
 
-function run_xinuk () {
-RUN_SCRIPT="java -Dmock.config.isSupervisor=$2  
--Dclustering.ip=$1 -Dclustering.port=${XINUK_PORT} 
--Dclustering.supervisor.ip=${RASPBERRY1} -Dclustering.supervisor.port=${XINUK_PORT} 
--Dclustering.min-nr-of-members=${MIN_NR_OF_MEMBERS} -Dmock.config.worldWidth=${WORLD_WIDTH}
--Dmock.config.workersX=${WORKERS_X} -Dmock.config.workersY=${WORKERS_Y} -Dmock.config.signalDisabled=${SIGNAL_DISABLED}
--Dmock.config.worldHeight=${WORLD_HEIGHT} -Dmock.config.guiType=${GUI_TYPE}
--Dmock.config.ledPanelPort=${LED_PANEL_PORT} -Dmock.config=workersRoot=${WORKERS_ROOT} 
--jar /home/pi/Desktop/xinuk/${APLICATION_NAME}/target/scala-2.13/${APLICATION_NAME}.jar"
+function run_xinuk() {
+    local deviceIp=${1}
+    local isSupervisor=${2}
+    RUN_SCRIPT="java 
+-Dmock.config.isSupervisor=${isSupervisor} -Dclustering.ip=${run_xinuk} -Dclustering.port=${XINUK_PORT} -Dclustering.supervisor.ip=${RASPBERRY1} -Dclustering.supervisor.port=${XINUK_PORT} 
+-Dclustering.min-nr-of-members=${MIN_NR_OF_MEMBERS} -Dmock.config.worldWidth=${WORLD_WIDTH} -Dmock.config.workersX=${WORKERS_X} -Dmock.config.workersY=${WORKERS_Y} 
+-Dmock.config.signalDisabled=${SIGNAL_DISABLED} -Dmock.config.worldHeight=${WORLD_HEIGHT} -Dmock.config.guiType=${GUI_TYPE} -Dmock.config.ledPanelPort=${LED_PANEL_PORT} 
+-Dmock.config=workersRoot=${WORKERS_ROOT}  -jar /home/pi/Desktop/xinuk/${APPLICATION_NAME}/target/scala-2.13/${APPLICATION_NAME}.jar"
 
-ssh -l ${USERNAME} $1 ${RUN_SCRIPT} "< /dev/null > /tmp/mylogfile 2>&1 &"
-echo Run Xinuk: ${APPLICATION_NAME} on $1
+    ssh -l ${USERNAME} $1 ${RUN_SCRIPT} "< /dev/null > /tmp/mylogfile 2>&1 &"
+    echo Run Xinuk: ${APPLICATION_NAME} on ${deviceIp}
 }
 
 echo Running Xinuk on every machine...
