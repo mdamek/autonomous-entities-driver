06aa25a45e6cd088e87624bc108cdbf007dd3e87
MarcinDamek <marcind70@gmail.com>
2021-04-06 13:28:14 +0200

    fix typo

--- scripts_runner.py
+++ scripts_runner.py
@@ -3,4 +3,4 @@ import os
 
 def restart_led_servers():
     subprocess.call("scripts/kill_led_servers.sh")
-    subprocess.call(os.path.normpath"scripts/run_led_servers.sh")
+    subprocess.call("scripts/run_led_servers.sh")
