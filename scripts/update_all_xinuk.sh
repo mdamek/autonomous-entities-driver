6c14d40d7e75988f85076ec3dc0a10934dff0d87
MarcinDamek <marcind70@gmail.com>
2021-04-19 19:01:54 +0200

    remove commas

--- scripts/update_all_xinuk.sh
+++ scripts/update_all_xinuk.sh
@@ -3,13 +3,13 @@ source $(dirname $0)/raspberry_hosts.sh
 
 USERNAME="pi"
 
-PROJECTS=("mock", "torch", "fortwist", "game", "rabbits")
+PROJECTS=("mock" "torch" "fortwist" "game" "rabbits")
 
 echo Updating Xinuk...
 echo $UPDATEXINUKSCRIPT
 for HOSTNAME in ${RASPBERRYHOSTS[@]} ; do
     ssh -l ${USERNAME} ${HOSTNAME} "cd Desktop/xinuk; git pull;"
     for PROJECT in ${PROJECTS[@]} ; do
-        ssh -l ${USERNAME} ${HOSTNAME} "sbt ${PROJECT}/assembly";
+        ssh -l ${USERNAME} ${HOSTNAME} "sbt ${PROJECT}/assembly < /dev/null > /tmp/mylogfile 2>&1 &";
     done
 done
