893b485a8dafb924a2e36c033823f92c466fedc1
MarcinDamek <marcind70@gmail.com>
2021-04-19 14:58:26 +0200

    add script to update all

---
+++ scripts/update_all_xinuk.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+source $(dirname $0)/raspberry_hosts.sh
+
+USERNAME="pi"
+
+PROJECTS=["mock", "torch", "fortwist", "game", "rabbits"]
+
+echo Updating Xinuk...
+echo $UPDATEXINUKSCRIPT
+for HOSTNAME in ${RASPBERRYHOSTS[@]} ; do
+    ssh -l ${USERNAME} ${HOSTNAME} "cd Desktop/xinuk; git pull;"
+    for PROJECT in ${PROJECTS[@]} ; do
+        ssh -l ${USERNAME} ${HOSTNAME} "sbt ${PROJECT}/assembly" "< /dev/null > /tmp/mylogfile 2>&1 &"
+done
