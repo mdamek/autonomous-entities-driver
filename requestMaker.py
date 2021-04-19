89762f38946377f1f3843924390c3d3e42e71f9b
MarcinDamek <marcind70@gmail.com>
2021-04-19 18:05:03 +0200

    add try catch on simulation

--- requestMaker.py
+++ requestMaker.py
@@ -6,17 +6,37 @@ core = "http://%s:%s/" % (supervisor_host, port_of_actors_manager)
 
 def start_stepped_simulation():
     path = core + "startSteppedSimulation"
-    requests.get(path, timeout=5)
+    try:
+        requests.get(path, timeout=5)
+    except requests.Timeout:
+        print("Timeout on: ", path)
+    except requests.ConnectionError:
+        print("Request error")
 
 def stop_stepped_simulation():
     path = core + "stopSteppedSimulation"
-    requests.get(path, timeout=5)
+    try:
+        requests.get(path, timeout=5)
+    except requests.Timeout:
+        print("Timeout on: ", path)
+    except requests.ConnectionError:
+        print("Request error")
 
 def make_iteration():
     path = core + "makeIteration"
-    requests.get(path, timeout=5)
+    try:
+        requests.get(path, timeout=5)
+    except requests.Timeout:
+        print("Timeout on: ", path)
+    except requests.ConnectionError:
+        print("Request error")
 
 def set_simulation_delay(delay):
     path = core + "setSimulationDelay"
     params = {'delay' : delay}
-    requests.get(path, params=params, timeout=5)
+    try:
+        requests.get(path, params=params, timeout=5)
+    except requests.Timeout:
+        print("Timeout on: ", path)
+    except requests.ConnectionError:
+        print("Request error")
