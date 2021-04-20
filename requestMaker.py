b84d84e6f5b2d2581258fc7ee42e13e74cb361d5
MarcinDamek <marcind70@gmail.com>
2021-04-21 00:26:46 +0200

    add usage requests

--- requestMaker.py
+++ requestMaker.py
@@ -3,11 +3,11 @@ import requests
 supervisor_host = "192.168.100.180"
 port_of_actors_manager = "8013"
 core = "http://%s:%s/" % (supervisor_host, port_of_actors_manager)
-
+timeouts = 5
 def start_stepped_simulation():
     path = core + "startSteppedSimulation"
     try:
-        requests.get(path, timeout=5)
+        requests.get(path, timeout=timeouts)
     except requests.Timeout:
         print("Timeout on: ", path)
     except requests.ConnectionError:
@@ -16,7 +16,7 @@ def start_stepped_simulation():
 def stop_stepped_simulation():
     path = core + "stopSteppedSimulation"
     try:
-        requests.get(path, timeout=5)
+        requests.get(path, timeout=timeouts)
     except requests.Timeout:
         print("Timeout on: ", path)
     except requests.ConnectionError:
@@ -25,7 +25,7 @@ def stop_stepped_simulation():
 def make_iteration():
     path = core + "makeIteration"
     try:
-        requests.get(path, timeout=5)
+        requests.get(path, timeout=timeouts)
     except requests.Timeout:
         print("Timeout on: ", path)
     except requests.ConnectionError:
@@ -35,8 +35,17 @@ def set_simulation_delay(delay):
     path = core + "setSimulationDelay"
     params = {'delay' : delay}
     try:
-        requests.get(path, params=params, timeout=5)
+        requests.get(path, params=params, timeout=timeouts)
     except requests.Timeout:
         print("Timeout on: ", path)
     except requests.ConnectionError:
         print("Request error")
+
+
+def start_motion_sensor(shape):
+#make request for motion sensor!
+    pass
+
+def stop_motion_sensor():
+#make request for motion sensor!
+    pass
